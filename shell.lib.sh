6acf83150a0e769c056c326dc675c95a3f3cb3ca
Rodolfo Granata <warlock.cc@gmail.com>
2013-08-20 19:41:23 -0400

    * add auto-completion to marks

--- shell.lib.sh
+++ shell.lib.sh
@@ -96,7 +96,7 @@ function highlight {
 
 
 export __MARKPATH=$HOME/.marks
-function jump {
+function j {
   cd -P $__MARKPATH/$1 2>/dev/null || echo "No such mark: $1"
 }
 function mark {
@@ -106,7 +106,19 @@ function unmark {
   rm -i $__MARKPATH/$1
 }
 function marks {
-  ls -l $__MARKPATH | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
+  ls -l $__MARKPATH | awk '/->/ {printf "%-15s -> %s\n", $9, $11}'
 }
+function _jcompgen {
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=()
+  if [ $(ls -1 $__MARKPATH 2>/dev/null | sed "/^$cur/!d" | wc -l) -eq 1 ]; then
+    COMPREPLY=($(ls -1 $__MARKPATH | sed "/^$cur/!d"))
+    return
+  fi
+  while read l; do
+    COMPREPLY=("${COMPREPLY[@]}" "$l")
+  done < <(marks | sed "/^$cur/!d")
+}
+complete -o nospace -F _jcompgen j
 
 # vim: set sw=2 sts=2 : #
