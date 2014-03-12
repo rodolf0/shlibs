d22e335c2b4545bc21c748c5f0bbec543403b542
Rodolfo Granata <warlock.cc@gmail.com>
2014-03-12 14:56:26 -0400

    add tbetween function

--- datetime.lib.sh
+++ datetime.lib.sh
@@ -3,7 +3,7 @@
 # do timezone conversions
 function tz_calc {
   if [ $# -lt 1 ]; then
-    echo "Usage: tz_calc <tz-dest> [<tm-str>] [<tz-orig>]"
+    echo "Usage: tz_calc <tz-dest> [<tm-str>] [<tz-orig>]" >&2
     return 1
   fi
 
@@ -25,4 +25,16 @@ function months_first_day {
 # get the last day of the current month (a format string can be appended)
 function months_last_day {
   date -d "-$(date +%d) days +1 month" "$@"
+}
+
+# check if timestring is between 2 date-times
+function tbetween {
+  [ $# -lt 2 ] && {
+  echo "usage: tbetween <start-date> <end-date> [timestr (defaults to now)]" >&2
+    return 1; }
+  local s=$(date +%s)
+  local strt="$1"; shift
+  local endt="$1"; shift
+  [ "$1" ] && s=$(date -d "$1" +%s)
+  [ $(date -d "$strt" +%s) -le $s -a $(date -d "$endt" +%s) -gt $s ]
 }
